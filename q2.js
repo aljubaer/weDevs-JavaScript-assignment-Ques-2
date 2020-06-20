63680ddae959eaaa29c87f3c54791a0e8bab7cd1
Abdullah Al Jubaer <bsse0812@iit.du.ac.bd>
2020-06-20 20:57:36 +0600

    update to set version

--- q2.js
+++ q2.js
@@ -1,10 +1,6 @@
 let arr = [1, 2, 2, 3, 4, 4, 5];
 
 let prev = -1;
-arr = arr.filter(item => {
-    let res = item != prev;
-    prev = item;
-    return res;
-});
+arr = Array.from(new Set(arr));
 
 console.log(arr);
